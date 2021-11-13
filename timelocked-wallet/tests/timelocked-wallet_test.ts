1d8b4e7d3dadcb712f77bfe5668e91fee1412b94
qxotk <1896689+qxotk@users.noreply.github.com>
2021-11-12 23:46:34 -0500

    Added stubs for all the other tests listed.

--- timelocked-wallet/tests/timelocked-wallet_test.ts
+++ timelocked-wallet/tests/timelocked-wallet_test.ts
@@ -37,11 +37,60 @@ Clarinet.test({
     }
 });
 
-// - does not allow anyone else to lock an amount
-// - cannot be locked more than once
-// - cannot set the unlock height to a value less than the current block height
-// - allow the beneficiary to bestow the right to claim to someone else
-// - does not allow anyone else to bestow the right to claim to someone else. (Not even the contract owner.)
-// - allows the beneficiary to claim the balance when the block height is reached
-// - does not allow the beneficiary to claim the balance before the block-height is reached
-// - only beneficiary can claim when block height is reached
+Clarinet.test({
+    name: "does not allow anyone else to lock an amount",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+    }
+});
+
+Clarinet.test({
+    name: "cannot be locked more than once",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+    }
+});
+
+Clarinet.test({
+    name: "cannot set the unlock height to a value less than the current block height",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+    }
+});
+
+Clarinet.test({
+    name: "allow the beneficiary to bestow the right to claim to someone else",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+    }
+});
+
+Clarinet.test({
+    name: "does not allow anyone else to bestow the right to claim to someone else. (Not even the contract owner.)",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+    }
+});
+
+
+Clarinet.test({
+    name: "allows the beneficiary to claim the balance when the block height is reached",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+    }
+});
+
+Clarinet.test({
+    name: "does not allow the beneficiary to claim the balance before the block-height is reached",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+    }
+});
+
+Clarinet.test({
+    name: "only beneficiary can claim when block height is reached",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+    }
+});
+
